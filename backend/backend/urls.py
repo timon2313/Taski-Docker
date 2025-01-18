20675a208d8df0d9c21f9b6cf44de71e3f0bdaaf
timon2313 <tima2313@gmail.com>
2025-01-19 02:07:16 +0300

    fix 3.0

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from rest_framework import routers
 from api import views
 
+
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
